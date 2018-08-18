29c5579a217ede4c5a1ca02998fc4eede6a41576
doitwld <doitwld@163.com>
2018-08-18 16:17:27 +0800

    thrid commit

--- python/genrentinfo.py
+++ python/genrentinfo.py
@@ -1,3 +1,4 @@
+#!encoding=utf-8
 import sys
 import urllib
 import urllib.request
@@ -7,6 +8,8 @@ import queue
 import jieba
 import threading
 import time
+import datetime
+import re
 
 httpHandler = urllib.request.HTTPHandler(debuglevel=1)
 httpsHandler = urllib.request.HTTPSHandler(debuglevel=1)
@@ -39,16 +42,14 @@ def multithread(func, q, number):
     for t in thread_list:
         t.join()
 
-def getrentinfo(url):
+def getrentinfo(baseurl):
   global rentinfo
   print("getrent start")
-  url = url + "discussion?start="
-  url = "https://www.douban.com/group/beijingzufang/discussion?start="
+  baseurl = "https://www.douban.com/group/beijingzufang/discussion?start="
   count = -1
   while True:
-    count += 10
-    url = url + str(25 * count)
-    print(url)
+    count += 1
+    url = baseurl + str(25 * count)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36", "Upgrade-Insecure-Requests":1, "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Cache-Control":"max-age=0", "Host":"www.douban.com"}
     request = urllib.request.Request(url = url, headers=headers)
     response = urllib.request.urlopen(request)
@@ -56,11 +57,44 @@ def getrentinfo(url):
     soup = BeautifulSoup(html, "html.parser")
     result = soup.select(".olt .title")
     for r in result:
-      print(r)
-      break
-    print("getrent over")
-    break
-  
+      data = []
+      data.append(r.a["href"])
+      title = r.a["title"]
+      data.append(title)
+      data.append(r.next_sibling.next_sibling.string)
+      num = r.next_sibling.next_sibling.next_sibling.next_sibling.string
+      if num==None:
+          data.append(-1)
+      else:
+          data.append(int(num))
+      lastreply = str(datetime.datetime.now().year) + "-" + r.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string
+      timeArray = time.strptime(lastreply, "%Y-%m-%d %H:%M")
+      data.append(int(time.mktime(timeArray)))
+      request = urllib.request.Request(url = r.a["href"], headers=headers)
+      response = urllib.request.urlopen(request)
+      html = response.read().decode("UTF-8")
+      soup = BeautifulSoup(html, "html.parser")
+      content = title
+      result = soup.select(".topic-richtext p") 
+      for r in result:
+          if r.string==None:
+              continue
+          content = content +  r.string.strip() + " " 
+      data.append(content)
+      info = []
+      GetInfo(title, content, info)
+      firstedit = soup.select(".color-green")[0].string
+      timeArray = time.strptime(firstedit, "%Y-%m-%d %H:%M:%S")
+      data.append(int(time.mktime(timeArray)))
+      print(data)
+      time.sleep(4)
+
+def GetInfo(title, content, info):
+  print(title)
+  ##判断主次卧
+  r1 = '[次卧]'
+  print(bool(re.search(r1, title)))
+ 
 def SortAndWriteToFile():
   output = open("../data/groupinfo.txt", "w")
   for group in groupinfo:
