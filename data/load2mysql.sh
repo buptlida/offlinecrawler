21a1ffdb3935fa6a738fd53367f26c34a0024e05
doitwld <doitwld@163.com>
2018-08-21 13:03:22 +0800

    commit

---
+++ data/load2mysql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+host=localhost
+port=3306
+user=root
+passwd=buptlida
+databases=rentinfo
+sql="load data local infile 'areanum.txt' replace into table region fields terminated by '\t' (regionname, code);"
+echo $sql
+mysql -h$host -P${port} -u$user -p$passwd -N -e "$sql" $databases
+
+sql="load data local infile 'subareanum.txt' replace into table street fields terminated by '\t' (streetname, code);"
+echo $sql
+mysql -h$host -P${port} -u$user -p$passwd -N -e "$sql" $databases
+
+sql="load data local infile 'subwaysnum.txt' replace into table subline fields terminated by '\t' (linename, code);"
+echo $sql
+mysql -h$host -P${port} -u$user -p$passwd -N -e "$sql" $databases
+
+sql="load data local infile 'subwaynum.txt' replace into table subway fields terminated by '\t' (name, code);"
+echo $sql
+mysql -h$host -P${port} -u$user -p$passwd -N -e "$sql" $databases
+
+
